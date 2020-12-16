a12b980b3eb77ba0a58fef3278697070d62ab24c
kalyangoud145 <kalyangoud145@gmail.com>
2020-12-16 17:59:50 +0530

    [Kalyan] added: logic to validate start date

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -42,6 +42,11 @@ class EmployeePayrollData {
     }
     get startDate() { return this._startDate; }
     set startDate(startDate) {
+        let now = new Date();
+        if (startDate > now) throw alert('Start Date is a Future Date!');
+        var diff = Math.abs(now.getTime() - startDate.getTime());
+        if (diff / (1000 * 60 * 60 * 24) > 30)
+            throw alert('Start Date is beyond 30 Days!');
         this._startDate = startDate;
     }
     // To string method
